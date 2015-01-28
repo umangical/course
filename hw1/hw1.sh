fe6d992ea5319e2d3b3ee7efa9829fab99dfa4ea
umangical <umang.methi@berkeley.edu>
2015-01-27 18:50:52 -0800

    Add python script

--- hw1/hw1.sh
+++ hw1/hw1.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # bash command-line arguments are accessible as $0 (the bash script), $1, etc.
 # echo "Running" $0 "on" $1
-echo "Replace the contents of this file with your solution."
+
+python hw1.py $1
+
+exit 0
